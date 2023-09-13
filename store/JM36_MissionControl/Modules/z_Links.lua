a902ad38ecdeb4111f9348bb7e11aeae877301c3
Jasen Samuels <jasensamuels@gmail.com>
2023-09-13 03:56:44 -0400

    Update z_Links.lua
    Fix invite

--- store/JM36_MissionControl/Modules/z_Links.lua
+++ store/JM36_MissionControl/Modules/z_Links.lua
@@ -1,4 +1,4 @@
 local MenuList = menu.my_root():list("Links", {}, "")
-MenuList:hyperlink("Discord Guild Invite", "https://discord.gg/vFb4b67f8q", "")
+MenuList:hyperlink("Discord Guild Invite", "https://discord.gg/ZbpNXmh2B4", "")
 MenuList:hyperlink("Donate via Ko-fi", "https://ko-fi.com/jaymontana36", "")
 MenuList:hyperlink("Donate via Liberapay", "https://liberapay.com/JayMontana36/donate", "")
